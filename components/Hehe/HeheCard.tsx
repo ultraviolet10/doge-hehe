4ecf07179b8a6f484dc6f4ccadae0ed39d8fda02
ultraviolet10 <aritrac1998@hotmail.com>
2022-10-29 09:16:36 +0530

    feat: mint nft

--- components/Hehe/HeheCard.tsx
+++ components/Hehe/HeheCard.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import { useWeb3 } from "../../hooks/useWeb3";
+import { useStore } from "@store/store";
 
 interface HeheCardProps {}
 
@@ -10,18 +11,31 @@ interface Haha {
 }
 
 const HeheCard: React.FC<HeheCardProps> = () => {
-  const { getHahas } = useWeb3();
+  const { store } = useStore();
+  const { account } = store;
+  const { getHahas, mintNft } = useWeb3();
   const [qa, setQa] = useState<Haha>({
     question: undefined,
     answer: undefined,
   });
+  const [minted, setMinted] = useState<boolean>(false);
 
   const handleHeheCommand = async () => {
+    setMinted(false);
     const haha = await getHahas();
     if (haha) {
       setQa({ question: haha[0], answer: haha[1] });
     }
   };
+
+  const handleMintClick = useCallback(async () => {
+    if (!qa.answer || !qa.question || !account) return;
+    const mintNew = await mintNft(qa.question, qa.answer, account);
+    if (mintNew) {
+      setMinted(true);
+      console.log("done");
+    }
+  }, [account, mintNft]);
 
   return (
     <div className="flex w-full h-[800px] items-center justify-center place-self-center">
@@ -41,16 +55,39 @@ const HeheCard: React.FC<HeheCardProps> = () => {
           Doge make hehe
         </span>
         {qa.answer ? (
-          <div className="flex flex-col space-y-4 py-4 ">
+          <div className="flex flex-col space-y-4 py-4">
             <span className="text-[16px] font-comic text-white">
               {qa.question}
             </span>
             <span className="text-[16px] font-comic text-white">
               {qa.answer}
             </span>
-            <button className="p-2 text-white border border-white rounded-lg">
-              Mint as NFT
-            </button>
+            <div className="flex flex-row space-x-2">
+              <button
+                className="w-1/2 p-2 text-white border border-white rounded-lg"
+                onClick={() => {
+                  handleHeheCommand();
+                }}
+              >
+                Go Again!
+              </button>
+              <button
+                className="w-1/2 p-2 text-white border border-white rounded-lg"
+                onClick={handleMintClick}
+              >
+                Mint as NFT
+              </button>
+            </div>
+            {minted ? (
+              <div className="flex flex-col">
+                <span className="text-[16px] font-comic text-bold text-white">
+                  {`Your token has been successfully minted!`}
+                </span>
+                <span className="text-[16px] font-comic text-bold text-white">
+                  {`Find your Hehe in your wallet.`}
+                </span>
+              </div>
+            ) : null}
           </div>
         ) : (
           <button
--- hooks/useWeb3.ts
+++ hooks/useWeb3.ts
@@ -2,9 +2,12 @@ import { useCallback, useMemo } from "react";
 import ABI from "../abi/abi.json";
 import { useStore } from "@store/store";
 
-import { HEHE_CONTRACT_ADDRESS } from "@config/config";
+import {
+  HEHE_CONTRACT_ADDRESS,
+  HEHE_TOKEN_CONTRACT_ADDRESS,
+} from "@config/config";
 
-import { ethers } from "ethers";
+import { ethers, ContractTransaction } from "ethers";
 
 export const useWeb3 = () => {
   const { store } = useStore();
@@ -14,8 +17,22 @@ export const useWeb3 = () => {
     return new ethers.Contract(HEHE_CONTRACT_ADDRESS, ABI.hehe, signer);
   }, [signer]);
 
+  const heheTokenContract = useMemo(() => {
+    return new ethers.Contract(
+      HEHE_TOKEN_CONTRACT_ADDRESS,
+      ABI.tokenMint,
+      signer
+    );
+  }, [signer]);
+
   const getHahas = useCallback(async () => {
-    const hahas = await heheContract.hahas(1);
+    const hahas = await heheContract.hahas(
+      Math.abs(
+        Math.floor(
+          Math.random() * (Math.ceil(0) - Math.floor(4)) + Math.ceil(0)
+        )
+      )
+    );
     if (hahas) {
       return hahas;
     } else {
@@ -23,7 +40,31 @@ export const useWeb3 = () => {
     }
   }, [heheContract]);
 
+  const mintNft = useCallback(
+    async (punchline: string, hook: string, address: string) => {
+      const mint: ContractTransaction = await heheTokenContract.create(
+        address,
+        punchline,
+        hook,
+        Math.abs(
+          Math.floor(
+            Math.random() * (Math.ceil(1) - Math.floor(10)) + Math.ceil(1)
+          )
+        )
+      );
+      if (mint) {
+        const mintTx = await mint.wait();
+        if (mintTx.confirmations > 0) {
+          return true;
+        }
+      }
+      return false;
+    },
+    [heheTokenContract]
+  );
+
   return {
     getHahas,
+    mintNft,
   };
 };
