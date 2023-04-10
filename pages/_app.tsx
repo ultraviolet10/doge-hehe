69e534082af2df2483dcec6e5b94ef682b85a890
ultraviolet10 <aritrac1998@hotmail.com>
2023-04-10 16:36:07 +0530

    [fix] image format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,7 +31,7 @@ const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
             />
             <meta
               property="og:image"
-              content={'/img/hehe.jpeg'}
+              content={'/img/hehe.jpg'}
             />
           </Head>
           <Script
